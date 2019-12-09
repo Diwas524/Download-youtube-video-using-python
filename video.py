f420f42d77cf7ed7a1f9853516b63abf9100be01
Diwas Pandey <34954384+Diwas524@users.noreply.github.com>
2019-12-09 17:10:31 +0545

    Add files via upload

---
+++ video.py
@@ -0,0 +1,33 @@
+import pafy
+
+#enter the url of the video you want to download
+url="https://www.youtube.com/watch?v=mkKXS0FI_L4"    # link of video url you want to download
+video=pafy.new(url)
+
+
+##to see the list of available streams for a video
+streams=video.streams
+for s in streams:
+    print(s)
+##To see all the formats
+for s in streams:
+    print(s.resolution,s.extension, s.get_filesize(), s.url)
+
+##get best resolution of the video
+best=video.getbest()
+print(best.resolution,best.extension)
+
+###best resolution in required format[give your req format in "preftype"]
+best = video.getbest(preftype="mp4")
+print(best.resolution, best.extension)
+
+#get url for download
+print(best.url)
+#download video
+#best.download(quiet=False) #this will download directly 
+#if you want to specify a particular path for the video
+filename = best.download(filepath="/home/diwas/Desktop/")     # change the location as per your need . 
+
+
+## credit :- aihubprojects.com
+
